93f672ada857224bc8184e4cc5a62a6fc9595b7c
Isaac Zakaria <isaac.zkr@gmail.com>
2024-06-18 22:58:53 -0700

    Update test.py

--- py/test.py
+++ py/test.py
@@ -12,6 +12,7 @@ def main():
     expt_dir = '../data/irixs/2024-6-6/CCD Scan 7009'
     expt_path = pathlib.Path(expt_dir)
 
+    # read and clean Andor info (AI) file
     ai_path = sorted(expt_path.glob('*AI.txt'))
     if len(ai_path) > 1:
         warnings.warn('More than one Andor info file found.')
@@ -21,8 +22,11 @@ def main():
     ds = ds.assign_coords({'excitation_energy': ds['BL 8 Energy']})
     ds = ds.swap_dims({'dim_0': 'excitation_energy'})
     ds = ds.drop_vars('dim_0')
-    print(ds)
+    #print(ds)
 
+    # read XES/RIXS traces
+    xes_path = expt_path.walk()
+    print(xes_path)
 
 if __name__ == '__main__':
     main()
